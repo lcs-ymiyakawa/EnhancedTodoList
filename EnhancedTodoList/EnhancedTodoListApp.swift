937007c4422b38f7dc7cfd4795a0f20e606a5eb3
Yoshi Miyakawa <ymiyakawa@lcs.on.ca>
2024-11-12 19:21:03 -0500

    Initial Commit

---
+++ EnhancedTodoList/EnhancedTodoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  EnhancedTodoListApp.swift
+//  EnhancedTodoList
+//
+//  Created by Yoshi Miyakawa on 2024/11/12.
+//
+
+import SwiftUI
+
+@main
+struct EnhancedTodoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ EnhancedTodoListTests/EnhancedTodoListTests.swift
@@ -0,0 +1,17 @@
+//
+//  EnhancedTodoListTests.swift
+//  EnhancedTodoListTests
+//
+//  Created by Yoshi Miyakawa on 2024/11/12.
+//
+
+import Testing
+@testable import EnhancedTodoList
+
+struct EnhancedTodoListTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
