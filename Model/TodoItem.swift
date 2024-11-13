90796e2fdaf81a73ad6fed7fe93d78f6b94d9efa
Yoshi Miyakawa <ymiyakawa@lcs.on.ca>
2024-11-12 19:33:41 -0500

    Added the initial data model.

---
+++ Model/TodoItem.swift
@@ -0,0 +1,39 @@
+//
+//  TodoItem.swift
+//  EnhancedTodoList
+//
+//  Created by Yoshi Miyakawa on 2024/11/12.
+//
+
+import Foundation
+ 
+struct TodoItem: Identifiable {
+    
+    let id = UUID()
+    var details: String
+    let createdOn: Date
+    var isCompleted: Bool
+    var completedOn: Date?
+    
+    init(
+        details: String,
+        createdOn: Date = Date(),
+        isCompleted: Bool = false,
+        completedOn: Date? = nil
+    ) {
+        self.details = details
+        self.createdOn = createdOn
+        self.isCompleted = isCompleted
+        self.completedOn = completedOn
+    }
+ 
+}
+ 
+let exampleData = [
+    
+    TodoItem(details: "Go for a walk"),
+    TodoItem(details: "Study for Physics"),
+    TodoItem(details: "Call mom"),
+    
+]
+
